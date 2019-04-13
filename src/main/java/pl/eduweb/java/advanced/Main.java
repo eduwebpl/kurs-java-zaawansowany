a5aa6bde521d3d20e91fec1c9711a251ed831a51
Rafał Solarski <solarski.rafal@gmail.com>
2019-04-13 15:39:41 +0200

    Lesson 0902-begin

---
+++ src/main/java/pl/eduweb/java/advanced/Main.java
@@ -0,0 +1,11 @@
+package pl.eduweb.java.advanced;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("ThreadId: " + Thread.currentThread().getId());
+        System.out.println("ThreadName: " + Thread.currentThread().getName());
+    }
+
+}
