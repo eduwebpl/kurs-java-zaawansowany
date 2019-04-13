ce072ee2246072341a49414b596723663b4e05b2
Rafał Solarski <solarski.rafal@gmail.com>
2019-04-13 15:40:43 +0200

    Lesson 1004-begin

---
+++ src/main/java/pl/eduweb/java/advanced/Main.java
@@ -0,0 +1,57 @@
+package pl.eduweb.java.advanced;
+
+
+import java.sql.*;
+
+public class Main {
+    public static final String JDBC_URL = "jdbc:sqlite:employees.db";
+
+    public static void main(String[] args) {
+        try (Connection connection = DriverManager.getConnection(JDBC_URL)) {
+            recreateTables(connection);
+            insertData(connection);
+            workOnDb(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void insertData(Connection connection) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("INSERT INTO employees (name) VALUES ('John')");
+            statement.executeUpdate("INSERT INTO employees (name) VALUES ('Kate')");
+            statement.executeUpdate("INSERT INTO employees (name) VALUES ('Jack')");
+            statement.executeUpdate("INSERT INTO secrets (value) VALUES ('Secret1234')");
+        }
+    }
+
+    private static void recreateTables(Connection connection) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("DROP TABLE IF EXISTS EMPLOYEES");
+            statement.executeUpdate("CREATE TABLE employees (id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)");
+            statement.executeUpdate("DROP TABLE IF EXISTS employees_notes");
+            statement.executeUpdate("CREATE TABLE employees_notes (id INTEGER PRIMARY KEY AUTOINCREMENT, value STRING)");
+            statement.executeUpdate("DROP TABLE IF EXISTS secrets");
+            statement.executeUpdate("CREATE TABLE secrets (id INTEGER PRIMARY KEY AUTOINCREMENT, value STRING)");
+        }
+    }
+
+    private static void workOnDb(Connection connection) throws SQLException {
+        var noteProvidedByUser = "Some really important thoughts";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("INSERT INTO employees_notes (value) VALUES ('" + noteProvidedByUser + "')");
+        }
+        printEmployeesNotes(connection);
+    }
+
+    private static void printEmployeesNotes(Connection connection) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM employees_notes");
+            while (resultSet.next()) {
+                Integer id = resultSet.getInt("id");
+                String name = resultSet.getString("value");
+                System.out.println(String.format("id=%d value=%s", id, name));
+            }
+        }
+    }
+}
