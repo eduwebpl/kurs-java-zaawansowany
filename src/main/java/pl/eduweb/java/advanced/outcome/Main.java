68e6f438b58148f5c765f2f492f7af8bb7940a69
Rafał Solarski <solarski.rafal@gmail.com>
2019-04-13 15:41:23 +0200

    Lesson 1105-finish

---
+++ src/main/java/pl/eduweb/java/advanced/outcome/Main.java
@@ -0,0 +1,9 @@
+package pl.eduweb.java.advanced.outcome;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/main/java/pl/eduweb/java/advanced/outcome/dump/OutcomeLoader.java
@@ -0,0 +1,11 @@
+package pl.eduweb.java.advanced.outcome.dump;
+
+import pl.eduweb.java.advanced.outcome.Outcome;
+
+import java.util.List;
+
+public interface OutcomeLoader {
+
+    List<Outcome> load();
+
+}
