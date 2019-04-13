cb5f4e942bcd9ca288fe478aff85899f1461a59f
Rafał Solarski <solarski.rafal@gmail.com>
2019-04-13 15:37:27 +0200

    Lesson 0204

---
+++ src/main/java/pl/eduweb/java/advanced/Main.java
@@ -0,0 +1,13 @@
+package pl.eduweb.java.advanced;
+
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            System.out.println("Tick");
+            Thread.sleep(2000);
+        }
+    }
+
+}
