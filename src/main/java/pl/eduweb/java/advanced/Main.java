039888cdc7cf1c5dcd1571d62fa5883db961631e
Rafał Solarski <solarski.rafal@gmail.com>
2019-04-13 15:38:58 +0200

    Lesson 0709-begin

---
+++ src/main/java/pl/eduweb/java/advanced/Main.java
@@ -0,0 +1,12 @@
+package pl.eduweb.java.advanced;
+
+
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var path = Paths.get("files");
+    }
+
+}
