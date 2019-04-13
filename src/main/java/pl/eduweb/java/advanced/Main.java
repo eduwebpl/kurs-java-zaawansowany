41465316700d0db76da48eb33979847aece0a798
Rafał Solarski <solarski.rafal@gmail.com>
2019-04-13 15:38:46 +0200

    Lesson 0707-begin

---
+++ src/main/java/pl/eduweb/java/advanced/Main.java
@@ -0,0 +1,19 @@
+package pl.eduweb.java.advanced;
+
+
+/*
+struct Class_File_Format {
+   u4 magic_number;
+   u2 minor_version;
+   u2 major_version;
+   ...
+}
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+        String filePath = "target/classes/pl/eduweb/java/advanced/Main.class";
+    }
+
+}
