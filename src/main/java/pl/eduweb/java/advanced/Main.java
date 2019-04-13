5e4cbe5a7c45aed17932e604adc9a5d86a962a79
Rafał Solarski <solarski.rafal@gmail.com>
2019-04-13 15:38:30 +0200

    Lesson 0704-finish

---
+++ src/main/java/pl/eduweb/java/advanced/Main.java
@@ -0,0 +1,21 @@
+package pl.eduweb.java.advanced;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("files/lorem.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
